c0f8e09c94edf1f9f18e507aeac375568dd11f9b
PabloAndres2343 <157170593+kloubpablo@users.noreply.github.com>
2026-03-02 22:02:30 -0600

    ARREGLOCOMERCIO

--- WebApplication/Models/Comercio.cs
+++ WebApplication/Models/Comercio.cs
@@ -1,34 +1,40 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplicationAPP.Models
 {
-    [Table("Comercio")]
+    [Table("COMERCIO_G4")]
     public class Comercio
     {
         [Key]
         public int IdComercio { get; set; }
 
         [Required]
+        [StringLength(50)]
         public string Identificacion { get; set; }
 
         public int TipoIdentificacion { get; set; }
 
         [Required]
+        [StringLength(150)]
         public string Nombre { get; set; }
 
         public int TipoDeComercio { get; set; }
 
-        public string Telefono { get; set; }
+        [StringLength(20)]
+        public string? Telefono { get; set; }
 
-        public string CorreoElectronico { get; set; }
+        [StringLength(150)]
+        public string? CorreoElectronico { get; set; }
 
-        public string Direccion { get; set; }
+        [StringLength(250)]
+        public string? Direccion { get; set; }
 
         public DateTime FechaDeRegistro { get; set; }
 
         public DateTime? FechaDeModificacion { get; set; }
 
-        public bool Estado { get; set; }
+        public bool Estado { get; set; } = true;
     }
 }
