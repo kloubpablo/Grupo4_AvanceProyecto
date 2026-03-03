bc463e6bb19f0324f03a2113c66d011a808ab021
PabloAndres2343 <157170593+kloubpablo@users.noreply.github.com>
2026-03-02 21:56:25 -0600

    ARREGLOSINPE

--- WebApplication/Models/Sinpe.cs
+++ WebApplication/Models/Sinpe.cs
@@ -1,17 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace WebApplication.Models
+namespace WebApplicationAPP.Models
 {
+    [Table("SINPE_G4")]
     public class Sinpe
     {
         [Key]
-        public int IdSinpe { get; set; }
+        public int Id { get; set; }
 
         [Required]
         [StringLength(10)]
         public string TelefonoOrigen { get; set; }
 
         [Required]
+        [StringLength(150)]
         public string NombreOrigen { get; set; }
 
         [Required]
@@ -19,15 +23,18 @@ namespace WebApplication.Models
         public string TelefonoDestinatario { get; set; }
 
         [Required]
+        [StringLength(150)]
         public string NombreDestinatario { get; set; }
 
         [Required]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Monto { get; set; }
 
         public DateTime FechaDeRegistro { get; set; }
 
+        [StringLength(250)]
         public string? Descripcion { get; set; }
 
-        public bool Estado { get; set; }
+        public bool Estado { get; set; } = true;
     }
 }
