4ec78860abcbc31418ed87a841aefba1bee47774
PabloAndres2343 <157170593+kloubpablo@users.noreply.github.com>
2026-04-08 22:16:23 -0600

    Api part 1

---
+++ WebApplication/Controllers/Api/BitacoraApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationAPP.Repositories;
+
+namespace WebApplicationAPP.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BitacoraApiController : ControllerBase
+    {
+        private readonly IBitacoraRepository _repo;
+
+        public BitacoraApiController(IBitacoraRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var lista = _repo.ObtenerTodos();
+            return Ok(lista);
+        }
+    }
+}
