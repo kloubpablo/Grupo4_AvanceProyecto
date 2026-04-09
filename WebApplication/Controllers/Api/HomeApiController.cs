f6d94213ad5da8ab9897bdac0574f08fe637a48f
PabloAndres2343 <157170593+kloubpablo@users.noreply.github.com>
2026-04-08 22:18:00 -0600

    API part 2

---
+++ WebApplication/Controllers/Api/HomeApiController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationAPP.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeApiController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                mensaje = "API funcionando correctamente",
+                sistema = "SINPE Empresarial"
+            });
+        }
+
+        [HttpGet("health")]
+        public IActionResult Health()
+        {
+            return Ok(new
+            {
+                estado = "OK",
+                servidor = DateTime.Now
+            });
+        }
+    }
+}
---
+++ WebApplication/Controllers/Api/LoginApiController.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApplication.Controllers.Api
+{
+    public class LoginApiController
+    {
+    }
+}
